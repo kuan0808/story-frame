ba4f6928578a86a8a781bdd0fb69cdc1fd4a2a08
kuan0808 <pp0923215826@gmail.com>
2024-03-24 18:32:21 +0800

    feat: add env var definitions

--- frontend/src/env.mjs
+++ frontend/src/env.mjs
@@ -5,12 +5,20 @@ import { createEnv } from "@t3-oss/env-nextjs";
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    BE_API_URL: z.string().min(1),
     NEYNAR_API_KEY: z.string().min(1),
   },
   client: {
-    NEXT_PUBLIC_API_URL: z.string().min(1),
+    NEXT_PUBLIC_BFF_API_URL: z.string().min(1),
+    NEXT_PUBLIC_WARPCAST_URI: z.string().min(1),
+    NEXT_PUBLIC_APP_URL: z.string().min(1),
   },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    BE_API_URL: process.env.BE_API_URL,
+    NEYNAR_API_KEY: process.env.NEYNAR_API_KEY,
+    NEXT_PUBLIC_BFF_API_URL: process.env.NEXT_PUBLIC_BFF_API_URL,
+    NEXT_PUBLIC_WARPCAST_URI: process.env.NEXT_PUBLIC_WARPCAST_URI,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
 });
