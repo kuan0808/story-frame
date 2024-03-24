cbb84a5f598adc3bf3bc3b45e84aef628af7fe0c
kuan0808 <pp0923215826@gmail.com>
2024-03-24 22:56:47 +0800

    fix: build errors

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
   },
 };
 
-export function Home() {
+export default function Home() {
   return (
     <div>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
